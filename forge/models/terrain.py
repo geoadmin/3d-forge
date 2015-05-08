de77ddb9d8eedf2287e08cda8107b3dab1cb5d0b
Gilbert Jeiziner <gjn@space.ch>
2015-05-08 10:57:33 +0200

    Create Terrain model and put read logic there

--- forge/models/terrain.py
+++ forge/models/terrain.py
@@ -1,57 +1,146 @@
 # -*- coding: utf-8 -*-
 
+from struct import unpack, calcsize
 from collections import OrderedDict
+from forge.lib.helpers import zigZagToNumber
+from forge.lib.decoders import unpackEntry, unpackIndices, decodeIndices
 
 
-## http://cesiumjs.org/data-and-assets/terrain/formats/quantized-mesh-1.0.html
-quantizedMeshHeader = OrderedDict([
-  ['centerX', 'd'], ## 8bytes
-  ['centerY', 'd'],
-  ['centerZ', 'd'],
-  ['minimumHeight', 'f'], ## 4bytes
-  ['maximumHeight', 'f'],
-  ['boundingSphereCenterX', 'd'],
-  ['boundingSphereCenterY', 'd'],
-  ['boundingSphereCenterZ', 'd'],
-  ['boundingSphereRadius', 'd'],
-  ['horizonOcclusionPointX', 'd'],
-  ['horizonOcclusionPointY', 'd'],
-  ['horizonOcclusionPointZ', 'd']
-])
+# http://cesiumjs.org/data-and-assets/terrain/formats/quantized-mesh-1.0.html
+class Meta:
+    quantizedMeshHeader = OrderedDict([
+        ['centerX', 'd'],  # 8bytes
+        ['centerY', 'd'],
+        ['centerZ', 'd'],
+        ['minimumHeight', 'f'],  # 4bytes
+        ['maximumHeight', 'f'],
+        ['boundingSphereCenterX', 'd'],
+        ['boundingSphereCenterY', 'd'],
+        ['boundingSphereCenterZ', 'd'],
+        ['boundingSphereRadius', 'd'],
+        ['horizonOcclusionPointX', 'd'],
+        ['horizonOcclusionPointY', 'd'],
+        ['horizonOcclusionPointZ', 'd']
+    ])
 
-vertexData = OrderedDict([
-  ['vertexCount', 'I'], ## 4bytes -> determines the size of the 3 following arrays
-  ['uVertexCount', 'H'], ## 2bytes, unsigned short
-  ['vVertexCount', 'H'],
-  ['heightVertexCount', 'H']
-])
+    vertexData = OrderedDict([
+        ['vertexCount', 'I'],  # 4bytes -> determines the size of the 3 following arrays
+        ['uVertexCount', 'H'],  # 2bytes, unsigned short
+        ['vVertexCount', 'H'],
+        ['heightVertexCount', 'H']
+    ])
 
-indexData16 = OrderedDict([
-  ['triangleCount', 'I'],
-  ['indices', 'H']
-])
-indexData32 = OrderedDict([
-  ['triangleCount', 'I'],
-  ['indices', 'I']
-])
+    indexData16 = OrderedDict([
+        ['triangleCount', 'I'],
+        ['indices', 'H']
+    ])
+    indexData32 = OrderedDict([
+        ['triangleCount', 'I'],
+        ['indices', 'I']
+    ])
 
-EdgeIndices16 = OrderedDict([
-  ['westVertexCount', 'I'],
-  ['westIndices', 'H'],
-  ['southVertexCount', 'I'],
-  ['southIndices', 'H'],
-  ['eastVertexCount', 'I'],
-  ['eastIndices', 'H'],
-  ['northVertexCount', 'I'],
-  ['northIndices', 'H']
-])
-EdgeIndices32 = OrderedDict([
-  ['westVertexCount', 'I'],
-  ['westIndices', 'I'],
-  ['southVertexCount', 'I'],
-  ['southIndices', 'I'],
-  ['eastVertexCount', 'I'],
-  ['eastIndices', 'I'],
-  ['northVertexCount', 'I'],
-  ['northIndices', 'I']
-])
+    EdgeIndices16 = OrderedDict([
+        ['westVertexCount', 'I'],
+        ['westIndices', 'H'],
+        ['southVertexCount', 'I'],
+        ['southIndices', 'H'],
+        ['eastVertexCount', 'I'],
+        ['eastIndices', 'H'],
+        ['northVertexCount', 'I'],
+        ['northIndices', 'H']
+    ])
+
+    EdgeIndices32 = OrderedDict([
+        ['westVertexCount', 'I'],
+        ['westIndices', 'I'],
+        ['southVertexCount', 'I'],
+        ['southIndices', 'I'],
+        ['eastVertexCount', 'I'],
+        ['eastIndices', 'I'],
+        ['northVertexCount', 'I'],
+        ['northIndices', 'I']
+    ])
+
+
+class Terrain:
+
+    def __init__(self):
+        self.header = OrderedDict()
+        for k, v in Meta.quantizedMeshHeader.iteritems():
+            self.header[k] = 0.0
+        self.u = []
+        self.v = []
+        self.h = []
+        self.indices = []
+        self.westI = []
+        self.southI = []
+        self.eastI = []
+        self.northI = []
+
+    def __str__(self):
+        str = 'Header: %s' % self.header
+        str += '\nVertexCount: %s' % len(self.u)
+        str += '\nuVertex: %s' % self.u
+        str += '\nvVertex: %s' % self.v
+        str += '\nhVertex: %s' % self.h
+        str += '\nindexData: %s' % self.indices
+        str += '\nwestIndicesCount: %s' % len(self.westI)
+        str += '\nwestIndices: %s' % self.westI
+        str += '\nsouthIndicesCount: %s' % len(self.southI)
+        str += '\nsouthIndices: %s' % self.southI
+        str += '\neastIndicesCount: %s' % len(self.eastI)
+        str += '\neastIndices: %s' % self.eastI
+        str += '\nnorthIndicesCount: %s' % len(self.northI)
+        str += '\nnorthIndices: %s' % self.northI
+        return str
+
+    def fromFile(self, filename):
+        self.__init__()
+        with open(filename, 'rb') as f:
+            for k, v in Meta.quantizedMeshHeader.iteritems():
+                size = calcsize(v)
+                self.header[k] = unpack('<%s' % v, f.read(size))[0]
+
+            vertexCount = unpackEntry(f, Meta.vertexData['vertexCount'])
+            for i in range(0, vertexCount):
+                self.u.append(zigZagToNumber(
+                    unpackEntry(f, Meta.vertexData['uVertexCount'])
+                ))
+            for i in range(0, vertexCount):
+                self.v.append(zigZagToNumber(
+                    unpackEntry(f, Meta.vertexData['vVertexCount'])
+                ))
+            for i in range(0, vertexCount):
+                self.h.append(zigZagToNumber(
+                    unpackEntry(f, Meta.vertexData['heightVertexCount'])
+                ))
+            # Indexes
+            triangleCount = unpackEntry(f, Meta.indexData16['triangleCount'])
+            self.indices = unpackIndices(
+                f, vertexCount, triangleCount * 3, Meta.indexData16['indices'], Meta.indexData32['indices']
+            )
+            self.indices = decodeIndices(self.indices)
+
+            # Read padding? Apparently we already reach the end the file at the moment
+
+            # Edges (vertices on the edge of the tile) indices
+            westIndicesCount = unpackEntry(f, Meta.EdgeIndices16['westVertexCount'])
+            self.westI = unpackIndices(
+                f, vertexCount, westIndicesCount, Meta.EdgeIndices16['westIndices'], Meta.EdgeIndices32['westIndices']
+            )
+            southIndicesCount = unpackEntry(f, Meta.EdgeIndices16['southVertexCount'])
+            self.southI = unpackIndices(
+                f, vertexCount, southIndicesCount, Meta.EdgeIndices16['southIndices'], Meta.EdgeIndices32['southIndices']
+            )
+            eastIndicesCount = unpackEntry(f, Meta.EdgeIndices16['eastVertexCount'])
+            self.eastI = unpackIndices(
+                f, vertexCount, eastIndicesCount, Meta.EdgeIndices16['eastIndices'], Meta.EdgeIndices32['eastIndices']
+            )
+            northIndicesCount = unpackEntry(f, Meta.EdgeIndices16['northVertexCount'])
+            self.northI = unpackIndices(
+                f, vertexCount, northIndicesCount, Meta.EdgeIndices16['northIndices'], Meta.EdgeIndices32['northIndices']
+            )
+
+            data = f.read(1)
+            if data:
+                raise Exception('Should have reached end of file, but didn\'t')
--- forge/scripts/terrain_reader.py
+++ forge/scripts/terrain_reader.py
@@ -1,79 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from struct import unpack, calcsize
-from collections import OrderedDict
-from forge.lib.helpers import zigZagToNumber
-from forge.lib.decoders import unpackEntry, unpackIndices, decodeIndices
-from forge.models.terrain import *
+from forge.models.terrain import Terrain
 
+ter = Terrain()
 
-#with open('forge/data/quantized-mesh/0.terrain', 'rb') as f:
-with open('forge/data/quantized-mesh/raron.flat.1.terrain', 'rb') as f:
-#with open('forge/data/quantized-mesh/goms.mountains.1.terrain', 'rb') as f:
-    header = OrderedDict()
-    for k, v in quantizedMeshHeader.iteritems():
-        size = calcsize(v)
-        header[k] = unpack('<%s' %v, f.read(size))[0]
-    print 'Header: %s' %header
-
-    vertexCount = unpackEntry(f, vertexData['vertexCount'])
-    # 3 list of size count
-    uVertex = []
-    for i in range(0, vertexCount):
-        uVertex.append(zigZagToNumber(
-            unpackEntry(f, vertexData['uVertexCount'])
-        ))
-    vVertex = []
-    for i in range(0, vertexCount):
-        vVertex.append(zigZagToNumber(
-            unpackEntry(f, vertexData['vVertexCount'])
-        ))
-    hVertex = []
-    for i in range(0, vertexCount):
-        hVertex.append(zigZagToNumber(
-            unpackEntry(f, vertexData['heightVertexCount'])
-        ))
-    print 'uVertex: %s' %uVertex
-    print 'vVertex: %s' %vVertex
-    print 'hVertex: %s' %hVertex
-
-    # Indexes
-    triangleCount = unpackEntry(f, indexData16['triangleCount'])
-    indexData = unpackIndices(
-        f, vertexCount, triangleCount*3, indexData16['indices'], indexData32['indices']
-    )
-    indexData = decodeIndices(indexData)
-    print 'indexData: %s' %indexData
-
-    # Read padding? Apparently we already reach the end the file at the moment
-
-    # Edges (vertices on the edge of the tile) indices
-    westIndicesCount = unpackEntry(f, EdgeIndices16['westVertexCount'])
-    westIndices = unpackIndices(
-        f, vertexCount, westIndicesCount, EdgeIndices16['westIndices'], EdgeIndices32['westIndices']
-    )
-    southIndicesCount = unpackEntry(f, EdgeIndices16['southVertexCount'])
-    southIndices = unpackIndices(
-        f, vertexCount, southIndicesCount, EdgeIndices16['southIndices'], EdgeIndices32['southIndices']
-    )
-    eastIndicesCount = unpackEntry(f, EdgeIndices16['eastVertexCount'])
-    eastIndices = unpackIndices(
-        f, vertexCount, eastIndicesCount, EdgeIndices16['eastIndices'], EdgeIndices32['eastIndices']
-    )
-    northIndicesCount = unpackEntry(f, EdgeIndices16['northVertexCount'])
-    northIndices = unpackIndices(
-        f, vertexCount, northIndicesCount, EdgeIndices16['northIndices'], EdgeIndices32['northIndices']
-    )
-
-    print 'westIndicesCount: %s' %westIndicesCount
-    print 'westIndices: %s' %westIndices
-    print 'southIndicesCount: %s' %southIndicesCount
-    print 'southIndices: %s' %southIndices
-    print 'eastIndicesCount: %s' %eastIndicesCount
-    print 'eastIndices: %s' %eastIndices
-    print 'northIndicesCount: %s' %northIndicesCount
-    print 'northIndices: %s' %northIndices
-
-    data = f.read(1)
-    if not data:
-        print 'end of file'
+# ter.fromFile('forge/data/quantized-mesh/0.terrain')
+ter.fromFile('forge/data/quantized-mesh/raron.flat.1.terrain')
+# ter.fromFile('forge/data/quantized-mesh/goms.mountains.1.terrain')
+print ter
