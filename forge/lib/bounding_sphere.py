6c8632e196942a6d9b714a3d3e98d798f9199b33
loicgasser <loicgasser4@gmail.com>
2015-05-11 15:23:05 +0200

    Compare naive approach to ritters sphere

--- forge/lib/bounding_sphere.py
+++ forge/lib/bounding_sphere.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import math
+from decimal import Decimal
 
 
 class BoundingSphere(object):
 
     def __init__(self, *args, **kwargs):
+        minVal = Decimal('-Infinity')
+        maxVal = Decimal('Infinity')
 
         self.center = map(float, kwargs.get('center', []))
         self.radius = float(kwargs.get('radius', 0))
+        self.minPoint = [maxVal, maxVal, maxVal]
+        self.maxPoint = [minVal, minVal, minVal]
 
     # Based on Ritter's algorithm
     def fromPoints(self, points):
@@ -21,9 +26,13 @@ class BoundingSphere(object):
         # Search a point b in P, which has the largest distance from a.
         b = self.furthestPoint(a, points)
 
-        # Initial center and radius
+        # Initial center and radius (Ritter)
         self.center = self.getMidPoint(a, b)
         self.radius = self.distance(a, b)
+
+        # Initial center and radius (naive)
+        naiveCenter = self.getMidPoint(self.minPoint, self.maxPoint)
+        naiveRadius = 0.0
 
         # Construct a new ball covering both point p and the previous ball
         for i in range(0, len(points)):
@@ -38,18 +47,44 @@ class BoundingSphere(object):
                     (newRadius * self.center[1] + oldToNew * point[1]) / distance,
                     (newRadius * self.center[2] + oldToNew * point[2]) / distance
                 ]
+            # Naive
+            naiveDistance = self.distance(naiveCenter, point)
+            if naiveDistance > naiveRadius:
+                naiveRadius = naiveDistance
+
+        # Keep the naive sphere if smaller
+        if naiveRadius < self.radius:
+            self.radius = naiveRadius
+            self.center = naiveCenter
 
     def getMidPoint(self, a, b):
         return [(a[0] + b[0]) / 2.0, (a[1] + b[1]) / 2.0, (a[2] + b[2]) / 2.0]
 
     def furthestPoint(self, pointA, points):
         maxDistance = 0.0
+
         for i in range(0, len(points)):
             point = points[i]
             distance = self.distance(point, pointA)
             if maxDistance < distance:
                 maxDistance = distance
                 furthestPoint = point
+
+            # Store the points containing the smallest and largest component
+            # Used for the naive approach
+            if point[0] < self.minPoint[0]:
+                self.minPoint[0] = point[0]
+            if point[1] < self.minPoint[1]:
+                self.minPoint[1] = point[1]
+            if point[2] < self.minPoint[2]:
+                self.minPoint[2] = point[2]
+            if point[0] > self.maxPoint[0]:
+                self.maxPoint[0] = point[0]
+            if point[1] > self.maxPoint[1]:
+                self.maxPoint[1] = point[1]
+            if point[2] > self.maxPoint[2]:
+                self.maxPoint[2] = point[2]
+
         return furthestPoint
 
     def distanceSquared(self, p1, p2):
