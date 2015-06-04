f9fe1a70c83897e1e9bdb38b0249f05b4a69a9b2
loicgasser <loicgasser4@gmail.com>
2015-06-04 15:49:43 +0200

    CamelCase names like everywhere else

--- forge/lib/boto_conn.py
+++ forge/lib/boto_conn.py
@@ -3,7 +3,7 @@
 from boto import connect_s3
 
 
-def _get_s3_conn(profileName='3dforge_filestorage'):
+def _getS3Conn(profileName='3dforge_filestorage'):
     try:
         conn = connect_s3(profile_name=profileName)
     except Exception as e:
@@ -11,12 +11,12 @@ def _get_s3_conn(profileName='3dforge_filestorage'):
     return conn
 
 
-conn_s3 = _get_s3_conn()
+connS3 = _getS3Conn()
 
 
-def get_bucket(bucketName='wroathiesiuxiefriepl-vectortiles'):
+def getBucket(bucketName='wroathiesiuxiefriepl-vectortiles'):
     try:
-        bucket = conn_s3.get_bucket(bucketName)
+        bucket = connS3.get_bucket(bucketName)
     except Exception as e:
         raise 'Error during connection %s' % e
     return bucket
