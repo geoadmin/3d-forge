a95a17d132dc602d3a4774da2e3f8100a3afeade
loicgasser <loicgasser4@gmail.com>
2015-06-04 15:53:06 +0200

    Must raise an Exception

--- forge/lib/boto_conn.py
+++ forge/lib/boto_conn.py
@@ -7,7 +7,7 @@ def _getS3Conn(profileName='3dforge_filestorage'):
     try:
         conn = connect_s3(profile_name=profileName)
     except Exception as e:
-        raise 'S3: Error during connection %s' % e
+        raise Exception('S3: Error during connection %s' % e)
     return conn
 
 
@@ -18,5 +18,5 @@ def getBucket(bucketName='wroathiesiuxiefriepl-vectortiles'):
     try:
         bucket = connS3.get_bucket(bucketName)
     except Exception as e:
-        raise 'Error during connection %s' % e
+        raise Exception('Error during connection %s' % e)
     return bucket
