ed2299f3c9db17d3cae64e6ed4aa85f867cc6dfd
loicgasser <loicgasser4@gmail.com>
2015-06-11 18:46:19 +0200

    More convinient way to handle params

--- forge/scripts/global_tiler.py
+++ forge/scripts/global_tiler.py
@@ -3,9 +3,16 @@
 import os
 from forge.lib.tiler import GlobalGeodeticTiler
 
+minLon = 7.814408987957777
+maxLon = 7.8720231243555085
+minLat = 46.68040307776367
+maxLat = 46.7345778113877
+minZoom = 2
+maxZoom = 17
 basePath = '/var/local/cartoweb/tmp/3dpoc/'
+
 # We already have level 0 and 1 (the whole globe)
-foldersName = range(2, 18)
+foldersName = range(minZoom, maxZoom + 1)
 getShapefiles = lambda x: x.endswith('Merge.shp')
 
 dataSourcePaths = []
@@ -14,10 +21,6 @@ for f in foldersName:
     shapefilesName = filter(getShapefiles, os.listdir(path))
     dataSourcePaths.append('%s%s/%s' % (basePath, f, shapefilesName[0]))
 
-minLon = 7.814408987957777
-maxLon = 7.8720231243555085
-minLat = 46.68040307776367
-maxLat = 46.7345778113877
 
-tiler = GlobalGeodeticTiler(minLon, maxLon, minLat, maxLat, 2, 17, dataSourcePaths)
+tiler = GlobalGeodeticTiler(minLon, maxLon, minLat, maxLat, minZoom, maxZoom, dataSourcePaths)
 tiler.createTiles()
