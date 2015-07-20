f22ffe2b61ff861cb5d228ed5d754985b38ef6f8
loicgasser <loicgasser4@gmail.com>
2015-07-20 17:37:27 +0200

    Deactivate sql logging for now

--- forge/lib/logs.py
+++ forge/lib/logs.py
@@ -7,9 +7,9 @@ import logging.config
 def getLogger(config, name, suffix=''):
     configFile = config.get('Logging', 'config')
     logFile = config.get('Logging', 'logfile')
-    sqlLogFile = config.get('Logging', 'sqlLogfile')
+    # sqlLogFile = config.get('Logging', 'sqlLogfile')
     logging.config.fileConfig(configFile, defaults=dict(
-        logfile=logFile % dict(timestamp=suffix),
-        sqlLogFile=sqlLogFile % dict(timestamp=suffix)
+        logfile=logFile % dict(timestamp=suffix)
+        # sqlLogFile=sqlLogFile % dict(timestamp=suffix)
     ))
     return logging.getLogger(name)
