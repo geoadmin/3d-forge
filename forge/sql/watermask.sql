dde7b8d7a19c538668bf7e1d5151c0f6c034f8fe
Loïc Gasser <loicgasser4@gmail.com>
2015-10-07 13:28:52 +0200

    Slight improvement of watermask

--- forge/sql/watermask.sql
+++ forge/sql/watermask.sql
@@ -13,65 +13,64 @@ DECLARE
     inside  integer;
     outside integer;
 BEGIN
-    IF EXISTS ( 
+    IF EXISTS (
         SELECT * FROM information_schema.columns
         WHERE table_name=watermask_table::text
         AND column_name=watermask_geom_column
-    ) THEN 
+    ) THEN
 
     -- this query returns a raster with a stable extent of 256x256 pixels
-    -- raster type is 1BB
-    -- pixel value 0: land 
-    -- pixel value 1: lake
+    -- raster type is 8BUI
+    -- pixel value 0: land
+    -- pixel value 255: lake
     -- if the tile geometry lies completely inside a lake a raster with one pixel with value 1 will be returned
     -- if the tile geometry lies completely outside the lakes a raster with one pixel with value 0 will be returned
     EXECUTE format('SELECT count(1) FROM %I where ST_ContainsProperly(%I,%L)',watermask_table,watermask_geom_column,bbox) INTO inside;
     EXECUTE format('SELECT count(1) FROM %I where ST_Intersects(%L,%I)',watermask_table,bbox,watermask_geom_column) INTO outside;
     IF outside = 0 THEN
         --RAISE NOTICE 'tile lies completely outside lakes';
-        sql := 'SELECT ST_AddBand(ST_MakeEmptyRaster(1,1,0,0,1,1 , 0, 0, 4326),''1BB''::text,0,0)';
+        sql := 'SELECT ST_AddBand(ST_MakeEmptyRaster(1, 1, 0, 0, 1, 1, 0, 0, 4326), ''8BUI''::text, 0, 0)';
         RETURN QUERY EXECUTE sql;
         RETURN;
     END IF;
-    
+
     IF inside > 0 THEN
         --RAISE NOTICE 'tile lies completely inside a lake';
-        sql := 'SELECT ST_AddBand(ST_MakeEmptyRaster(1,1,0,0,1,1 , 0, 0, 4326),''1BB''::text,1,0)';
+        sql := 'SELECT ST_AddBand(ST_MakeEmptyRaster(1, 1, 0, 0, 1, 1, 0, 0, 4326), ''8BUI''::text, 255, 0)';
         RETURN QUERY EXECUTE sql;
         RETURN;
     END IF;
 
+    -- ST_Union not needed for single band rasters
     sql := '
-    WITH input as (
-        SELECT ST_AddBand(ST_MakeEmptyRaster('|| width ||', '|| height ||', '|| xmin ||', '|| ymax ||', '|| scalex ||', '|| scaley ||', 0, 0, 4326),''1BB''::text,1,0) raster 
+    WITH input AS (
+        SELECT ST_AddBand(ST_MakeEmptyRaster('|| width ||', '|| height ||', '|| xmin ||', '|| ymax ||', '|| scalex ||', '|| scaley ||', 0, 0, 4326), ''8BUI''::text, 255, 0) AS raster
     ),
-    intersected as ( 
-        SELECT 
-        ST_Union(ST_AsRaster(ST_Intersection('|| watermask_geom_column ||',ST_Envelope(raster)),raster,''1BB'',1,0,true)) as raster 
-        FROM '|| watermask_table ||' vector, input
-        WHERE ST_Intersects(vector.'|| watermask_geom_column ||',ST_Envelope(input.raster))
+    intersected AS (
+        SELECT
+          ST_AsRaster(ST_Intersection('|| watermask_geom_column ||', ST_Envelope(raster)), raster, ''8BUI''::text, 255, 0, true) AS raster
+          FROM '|| watermask_table ||' AS vector, input
+          WHERE ST_Intersects(vector.'|| watermask_geom_column ||', ST_Envelope(input.raster))
     )
-    SELECT 
-        ST_MapAlgebra(
-        input.raster
-        , 1
-        , intersected.raster
-        , 1
-        , ''[rast2.val] + [rast1.val]''
-        , ''1BB''
-        , ''FIRST''
-        , NULL
-        , NULL
-        , NULL)
-    FROM 
-        input, intersected 
+    SELECT ST_MapAlgebra(
+        r1.raster, 1,
+        r2.raster, 1,
+        ''[rast2.val]''::text,
+        ''8BUI''::text,
+        ''FIRST''::text,
+        ''0''::text,
+        ''0''::text,
+        0
+    )
+    FROM input AS r1
+    CROSS JOIN intersected AS r2
     ';
 
-    --RAISE NOTICE 'function parameters: xmin: % ymin: % xmax: % ymax: % scalex: % scaley: % watermask_table: % watermask_column % ', xmin,ymin,xmax,ymax,scalex,scaley,watermask_table,watermask_geom_column;
+    --RAISE NOTICE 'function parameters: xmin: % ymin: % xmax: % ymax: % scalex: % scaley: % watermask_table: % watermask_column % ',xmin,ymin,xmax,ymax,scalex,scaley,watermask_table,watermask_geom_column;
     --RAISE NOTICE 'sql: %',sql;
     RETURN QUERY EXECUTE sql;
     ELSE
-        RAISE NOTICE 'could not open column % in table %',watermask_geom_column,watermask_table;
+        RAISE NOTICE 'could not open column % in table % ',watermask_geom_column,watermask_table;
     END IF;
   
 END
